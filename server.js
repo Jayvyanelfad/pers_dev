f98e3c3cea21be0e96f3023b8c7d9ec2872e2b1c
Jayvyanelfad <jayvyanelf@gmail.com>
2024-11-14 12:56:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(cors());
 
 const uri = "mongodb+srv://PersonalDevelopmentAcademyUser:Vihoutou2002#@personaldevelopmentacad.txpcn.mongodb.net/?retryWrites=true&w=majority&appName=PersonalDevelopmentAcademyCluster";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,13 +19,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
@@ -54,6 +50,7 @@ app.post('/rate', async (req, res) => {
     res.send('Rating submitted!');
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
