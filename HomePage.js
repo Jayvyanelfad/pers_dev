4934fd44acfb43c10e4a9ee58df0475c7528968e
Jayvyanelfad <jayvyanelf@gmail.com>
2024-11-14 07:52:57 +0530

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const HomePage = () => {
-  return (
-    <div>
-      <h1>Welcome to Personal Development Academy</h1>
-      <p>Your journey to self-improvement starts here.</p>
-    </div>
-  );
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Personal Development Academy</h1>
+            <p>Your journey to personal growth begins here.</p>
+            <Link to="/help">Need Help?</Link>
+        </div>
+    );
 };
 
-export default HomePage;
+export default Home;
