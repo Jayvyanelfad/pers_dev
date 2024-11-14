fac75629c04eb57b8365b5fd662adc91aea2b2e6
Jayvyanelfad <jayvyanelf@gmail.com>
2024-11-14 19:14:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,12 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
-const helpRoute = require('./HelpRoute');
-const helpAPI = require('./HelpAPI');
-const contactUsRoute = require('./ContactUsRoute');
-const contactUsAPI = require('./ContactUsAPI');
-const rateContentRoute = require('./RateContentRoute');
-const rateContentAPI = require('./RateContentAPI');
-
 app.use(express.json());
 app.use(cors());
 
 const uri = "mongodb+srv://PersonalDevelopmentAcademyUser:Vihoutou2002#@personaldevelopmentacad.txpcn.mongodb.net/?retryWrites=true&w=majority&appName=PersonalDevelopmentAcademyCluster";
 
+// MongoDB Client to ensure connection
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,20 +20,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Connect the client to the server
     await client.connect();
+    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
+    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
 run().catch(console.dir);
 
+// Mongoose connection
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+// Schema and Model for courses
 const courseSchema = new mongoose.Schema({
     name: String,
     description: String
@@ -47,6 +46,7 @@ const courseSchema = new mongoose.Schema({
 
 const Course = mongoose.model('Course', courseSchema);
 
+// API Endpoints
 app.get('/courses', async (req, res) => {
     const courses = await Course.find();
     res.send(courses);
@@ -56,13 +56,6 @@ app.post('/rate', async (req, res) => {
     const { course, rating, comment } = req.body;
     res.send('Rating submitted!');
 });
-
-app.use(helpRoute);
-app.use(helpAPI);
-app.use(contactUsRoute);
-app.use(contactUsAPI);
-app.use(rateContentRoute);
-app.use(rateContentAPI);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
